e426b4b40a9c7747db143777f35a9570997ba730
Milan <milanpanda4425@gmail.com>
2022-11-17 01:14:34 +0530

    first commit

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img
+        className="headerImg"
+        src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt=""
+      />
+    </div>
+  );
+}
